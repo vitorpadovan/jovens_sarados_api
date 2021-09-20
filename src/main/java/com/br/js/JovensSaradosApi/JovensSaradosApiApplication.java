382cd80d1e938701e337af0dd1b4f9b3c5c8a63c
Vitor Hugo Padovan Hernandes <vitor.padovan89@gmail.com>
2021-09-19 21:11:44 -0300

    Adicionando Controlador inicial

--- src/main/java/com/br/js/JovensSaradosApi/JovensSaradosApiApplication.java
+++ src/main/java/com/br/js/JovensSaradosApi/JovensSaradosApiApplication.java
@@ -2,20 +2,21 @@ package com.br.js.JovensSaradosApi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.stereotype.*;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+@Controller
 @SpringBootApplication
 public class JovensSaradosApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JovensSaradosApiApplication.class, args);
 	}
-	
-	@RequestMapping("/")
-    @ResponseBody
-    String home() {
-      return "Api Jovens Sarados";
-    }
 
+	@RequestMapping("/")
+	@ResponseBody
+	String home() {
+		return "Api Jovens Sarados";
+	}
 }
