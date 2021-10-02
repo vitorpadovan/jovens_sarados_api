29834c4807fe68ff71ca13b4d5d6087fbdd03fc5
Vitor Hugo Padovan Hernandes <vitor.padovan89@gmail.com>
2021-10-02 09:10:32 -0300

    Adicionando versionamento da API no Swagger

--- src/main/java/com/br/js/JovensSaradosApi/config/swagger/SwaggerProd.java
+++ src/main/java/com/br/js/JovensSaradosApi/config/swagger/SwaggerProd.java
@@ -3,6 +3,7 @@ package com.br.js.JovensSaradosApi.config.swagger;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
@@ -13,8 +14,8 @@ public class SwaggerProd {
 
 	@Bean
 	public Docket jovensSaradosApiSwagger() {
-		return new Docket(DocumentationType.SWAGGER_2).select()
-				.apis(RequestHandlerSelectors.basePackage("com.br.js.JovensSaradosApi")).paths(PathSelectors.ant("/**"))
-				.build();
+		return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.any())
+				.paths(PathSelectors.ant("/**")).build().apiInfo(new ApiInfoBuilder().title("Api Jovens Sarados")
+						.version(getClass().getPackage().getImplementationVersion()).build());
 	}
 }
